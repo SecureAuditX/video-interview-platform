a04b08236577a67cdea6612aface09b4d805f141
SecureAuditX <abdulkarimumar86@gmail.com>
2026-01-28 17:57:43 +0800

    database and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express, { response } from 'express'
 import { ENV } from './lib/env.js';
 import path from "path"
+import { connectDB } from './lib/db.js';
 
 const app =  express()
 const __dirname = path.resolve()
@@ -21,4 +22,13 @@ if (ENV.NODE_ENV === "production"){
     })
 }
 
-app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`))
+const startServer = async () => {
+    try {
+       await connectDB();
+       app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT))
+    } catch (error) {
+        console.error("Error starting the server:", error)
+    }
+}
+
+startServer()
